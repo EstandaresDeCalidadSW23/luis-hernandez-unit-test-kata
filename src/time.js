6ce001c5dd1e316ed0ad3d74589a893ad6112567
luisher25san <luisher25san@gmail.com>
2023-09-28 13:04:57 -0700

    correciones del ejercicio 'time'

--- src/time.js
+++ src/time.js
@@ -1,35 +1,44 @@
 function time(seconds) {
-    if (seconds >= 31536000) {
-      const years = Math.floor(seconds / 31536000);
-      seconds %= 31536000; 
-      return years + "y";
-    }
-    if (seconds >= 604800) {
-      const weeks = Math.floor(seconds / 604800);
-      seconds %= 604800; 
-      return weeks + "w";
-    }
-    if (seconds >= 86400) {
-      const days = Math.floor(seconds / 86400);
-      seconds %= 86400; 
-      return days + "d";
-    }
-    if (seconds >= 3600) {
-      const hours = Math.floor(seconds / 3600);
-      seconds %= 3600; 
-      return hours + "h";
-    }
-    if (seconds >= 60) {
-      const minutes = Math.floor(seconds / 60);
-      seconds %= 60; 
-      return minutes + "m";
-    }
-    return seconds + "s";
+  let result = "";
+
+  if (seconds >= 31536000) {
+    const years = Math.floor(seconds / 31536000);
+    seconds %= 31536000; // Restar los segundos equivalentes a los años
+    result += years + "y ";
   }
   
-  module.exports = time;
+  if (seconds >= 604800) {
+    const weeks = Math.floor(seconds / 604800);
+    seconds %= 604800; // Restar los segundos equivalentes a las semanas
+    result += weeks + "w ";
+  }
   
+  if (seconds >= 86400) {
+    const days = Math.floor(seconds / 86400);
+    seconds %= 86400; // Restar los segundos equivalentes a los días
+    result += days + "d ";
+  }
+  
+  if (seconds >= 3600) {
+    const hours = Math.floor(seconds / 3600);
+    seconds %= 3600; // Restar los segundos equivalentes a las horas
+    result += hours + "h ";
+  }
+  
+  if (seconds >= 60) {
+    const minutes = Math.floor(seconds / 60);
+    seconds %= 60; // Restar los segundos equivalentes a los minutos
+    result += minutes + "m ";
+  }
+  
+  if (seconds > 0) {
+    result += seconds + "s";
+  }
 
+  return result.trim() || "0s"; // Devolver "0s" si no se agregó ninguna unidad
+}
+
+module.exports = time;
 
 
 
